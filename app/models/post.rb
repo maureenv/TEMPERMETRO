1ddd833a9bed4f95e8f6498a81ce904eb4fcd1b4
Maureen VOGEL <maureenvogel@Maureen-VOGELs-MacBook-Pro.local>
2016-03-21 15:45:16 -0400

    adding users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy #now I can delete posts that have comments
+  validates :color, presence: true
   validates :title, presence: true,
                     length: {minimum: 1}
   validates :text, presence: true,
                     length: {minimum: 1}
+  belongs_to :user
 end
 
 #the above makes sure that title and text are filled out
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  resources :users
     # A resource is the term used for a collection of similar objects, such as articles, people or animals. You can create, read, update and destroy items for a resource and these operations are referred to as CRUD operations.
   root 'home#home'
 

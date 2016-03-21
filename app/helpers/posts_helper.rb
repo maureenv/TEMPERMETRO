10a40c1ccf74754107864a25bc3487a3e6e6480f
Maureen VOGEL <maureenvogel@Maureen-VOGELs-MacBook-Pro.local>
2016-03-21 09:59:35 -0400

    Posts now have different colors depending on the metro line

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,20 @@
 module PostsHelper
+  def post_color(post)
+    (if post.color == "red"
+  "<div class = 'postgradient_red'>"
+  elsif post.color =="orange"
+    "<div class = 'postgradient_orange'>"
+  elsif post.color =="yellow"
+    "<div class = 'postgradient_yellow'>"
+  elsif post.color =="green"
+    "<div class = 'postgradient_green'>"
+  elsif post.color =="blue"
+    "<div class = 'postgradient_blue'>"
+  elsif post.color =="silver"
+    "<div class = 'postgradient_silver'>"
+  end).html_safe
+
+  end
+
+
 end
